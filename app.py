abe15651a5d3a6849fdf9a4a62f2dc396e180cf4
varshinidovari <varshinidovari2@gmail.com>
2026-04-07 21:57:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ app = Flask(__name__)
 
 env = DeliveryEnv()
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return "AI Delivery Optimization Running"
 
 @app.route("/reset", methods=["POST"])
 def reset():
     state = env.reset()
-    return jsonify(state)
+    return jsonify({
+        "state": state
+    })
 
 @app.route("/step", methods=["POST"])
 def step():
-    action = request.json.get("action")
+    data = request.get_json()
+    action = data.get("action")
+
     state, reward, done = env.step(action)
 
     return jsonify({
